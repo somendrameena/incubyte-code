55c91ea260f5441ea860585fd34f160e6fb7d87b
Somendra Meena <somendrameena03@gmail.com>
2024-09-02 13:54:35 +0530

    added test cases for blank string, zeroes

--- tests.py
+++ tests.py
@@ -1,7 +1,21 @@
 import unittest
 
+from utils import add
+
+
 class AddTestCase(unittest.TestCase):
     """
     Test cases for add function
     """
-    pass
+
+    def test_blank_string(self):
+        result = add("")
+        self.assertEqual(result, 0)
+    
+    def test_zero(self):
+        result = add("0")
+        self.assertEqual(result, 0)
+    
+    def test_zeroes(self):
+        result = add("0,0,0")
+        self.assertEqual(result, 0)
