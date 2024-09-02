f061409a06c906e6c79cfcd185933c8600e58c1e
Somendra Meena <somendrameena03@gmail.com>
2024-09-02 15:29:48 +0530

    removed duplicate test cases

--- tests.py
+++ tests.py
@@ -22,23 +22,11 @@ class AddTestCase(unittest.TestCase):
         result = add("0,0,0")
         self.assertEqual(result, 0)
     
-    def test_nums_1(self):
-        result = add("0,1,2")
-        self.assertEqual(result, 3)
-    
-    def test_nums_2(self):
-        result = add("1,2,3,4")
-        self.assertEqual(result, 10)
-    
-    def test_nums_3(self):
+    def test_small_nums(self):
         result = add("3,14,7,42,71")
         self.assertEqual(result, 137)
     
-    def test_large_nums_1(self):
-        result = add("221,446,163,94")
-        self.assertEqual(result, 924)
-    
-    def test_large_nums_2(self):
+    def test_large_nums(self):
         result = add("3134,495,99412,9834")
         self.assertEqual(result, 112875)
     
